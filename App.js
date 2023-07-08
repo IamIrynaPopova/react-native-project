b177147a71814f0d66d4e204d42043a6413bbb68
Iryna Popova <popovaira969@gmail.com>
2023-07-08 18:02:02 +0300

    ADDED: UPDATED: registration form

--- App.js
+++ App.js
@@ -6,11 +6,33 @@ import {
   Text,
   View,
   TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  Dimensions,
 } from "react-native";
 
 export default function App() {
-  // const [text, onChangeText] = React.useState("");
-  // const [number, onChangeNumber] = React.useState("");
+  console.log(Platform.OS);
+  const [isShowKeyboard, setIsShowKeyboard] = React.useState(false);
+  const [formHeight, setFormHeight] = React.useState(549);
+  console.log(isShowKeyboard);
+  console.log(formHeight);
+  React.useEffect(() => {
+    const keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      () => {
+        setIsShowKeyboard(false);
+        setFormHeight(549);
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+    };
+  }, []);
+  const d = Dimensions.get("window");
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -18,30 +40,53 @@ export default function App() {
         resizeMode="cover"
         style={styles.image}
       >
-        <View style={styles.form}>
-          <Text style={styles.titleText}>Реєстрація</Text>
-          <View style={{ marginBottom: 16 }}>
-            <TextInput
-              style={styles.input}
-              // onChangeText={onChangeText}
-              // value={text}
-              placeholder="Логін"
-            />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+          <View
+            style={{
+              ...styles.form,
+              height: formHeight,
+            }}
+          >
+            <Text style={styles.titleText}>Реєстрація</Text>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                style={styles.input}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                  setFormHeight(631);
+                }}
+                placeholder="Логін"
+              />
+            </View>
+            <View style={{ marginBottom: 16 }}>
+              <TextInput
+                style={styles.input}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                  setFormHeight(631);
+                }}
+                placeholder="Адреса електронної пошти"
+              />
+            </View>
+            <View>
+              <TextInput
+                style={styles.input}
+                onFocus={() => {
+                  setIsShowKeyboard(true);
+                  setFormHeight(631);
+                }}
+                placeholder="Пароль"
+                secureTextEntry={true}
+              />
+            </View>
+            <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+              <Text style={styles.titleButton}>Зареєстуватися</Text>
+            </TouchableOpacity>
+            <Text style={styles.text}>Вже є акаунт? Увійти</Text>
           </View>
-          <View style={{ marginBottom: 16 }}>
-            <TextInput
-              style={styles.input}
-              placeholder="Адреса електронної пошти"
-            />
-          </View>
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Пароль"
-              secureTextEntry={true}
-            />
-          </View>
-        </View>
+        </KeyboardAvoidingView>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -52,8 +97,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   image: {
     flex: 1,
@@ -61,9 +104,9 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   form: {
+    position: "relative",
     backgroundColor: "#fff",
     paddingHorizontal: 16,
-    height: 549,
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
@@ -87,5 +130,34 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#E8E8E8",
     borderRadius: 5,
+  },
+  button: {
+    marginTop: 43,
+    paddingLeft: 32,
+    paddingTop: 16,
+    paddingRight: 32,
+    paddingBottom: 16,
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: 100,
+    backgroundColor: "#FF6C00",
+  },
+  titleButton: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: 400,
+  },
+  text: {
+    marginTop: 16,
+    marginBottom: 66,
+    color: "#1B4371",
+    textAlign: "center",
+    fontFamily: "Roboto",
+    fontSize: 16,
+    fontStyle: "normal",
+    fontWeight: 400,
   },
 });
